fe4debf1e0c92b5bd8fcad639c89efcd64e2ab72
Srikanta <srnagar@microsoft.com>
2021-05-20 14:22:51 -0700

    Receive sample for Service Bus

--- src/main/java/io/sample/maven/project/trace/servicebus/CollectServiceBusSendTraceAgent.java
+++ src/main/java/io/sample/maven/project/trace/servicebus/CollectServiceBusSendTraceAgent.java
@@ -1,8 +1,12 @@
 package io.sample.maven.project.trace.servicebus;
 
+import com.azure.core.util.IterableStream;
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.messaging.servicebus.ServiceBusSessionReceiverClient;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
 
 public class CollectServiceBusSendTraceAgent {
 
@@ -16,19 +20,35 @@ public class CollectServiceBusSendTraceAgent {
     }
 
     private static void doClientWork() throws InterruptedException {
+
+        String serviceBusConnectionString = "";
         String[] sessions = {"soccer", "cricket", "tennis", "badminton"};
         ServiceBusSenderClient serviceBusSenderClient = new ServiceBusClientBuilder()
-                .connectionString("")
+                .connectionString(serviceBusConnectionString)
                 .sender()
                 .buildClient();
 
         int i = 0;
         while (i < 4) {
-            i++;
             ServiceBusMessage message = new ServiceBusMessage("hello " + i)
                     .setMessageId(String.valueOf(i))
                     .setSessionId(sessions[i % 4]);
             serviceBusSenderClient.sendMessage(message);
+            i++;
         }
+        System.out.println("Sent 4 messages");
+
+        ServiceBusSessionReceiverClient serviceBusReceiverClient = new ServiceBusClientBuilder()
+                .connectionString(serviceBusConnectionString)
+                .sessionReceiver()
+                .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+                .queueName("test-session-sb")
+                .buildClient();
+        IterableStream<ServiceBusReceivedMessage> serviceBusReceivedMessages = serviceBusReceiverClient
+                .acceptSession("tennis")
+                .receiveMessages(4);
+        serviceBusReceivedMessages.forEach(message -> System.out.println("Received message " + message.getMessageId()));
+        serviceBusSenderClient.close();
+        serviceBusReceiverClient.close();
     }
 }
